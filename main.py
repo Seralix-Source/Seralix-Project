549c433109047d5c04caeb3caacf3575f1cf636c
LoganTaurus <logantaurus01@gmail.com>
2026-05-10 11:11:29 -0500

    Addition of fft-updated.slx trying the approach of cyclotomatic for only int coeffs

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from rich.pretty import pprint
+
 import slx.runner
 
 if __name__ == '__main__':
     with open('src/fft.slx') as stream:
+        pprint("Running FFT")
+        slx.runner.run(stream.read())
+    with open('src/fft-updated.slx') as stream:
+        pprint("Running FFT-UPDATED")
         slx.runner.run(stream.read())
     with open('src/ntt.slx') as stream:
+        pprint("Running NTT")
         slx.runner.run(stream.read())
     with open('src/ntt-updated.slx') as stream:
+        pprint("Running NTT-UPDATED")
         slx.runner.run(stream.read())
